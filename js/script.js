67fc0757d0106398792288b67e8e0880e768d458
ByteOfWaffle <156892485+ByteOfWaffle@users.noreply.github.com>
2025-05-06 11:18:42 +0200

    HTML opprydding og darkmode fiks
    Fikset på html struktur og flyttet ut css og javascript, fikk også darkmode til å funke

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 function sendMelding() {
     alert("Melding sendt!");
 }
+
+function darkmode() {
+    document.body.classList.toggle("dark-mode");
+    // Lagre vaglt modus i localstorage
+    localStorage.setItem('darkMode', document.body.classList.contains('dark-mode'));
+}
+
+// Sjekker hvilgen av modusene er lagret i localstorage
+document.addEventListener('DOMContentLoaded', function() {
+    if (localStorage.getItem('darkMode') === 'true') {
+        document.body.classList.add('dark-mode');
+    }
+});
+// Legger til eventlistener for å sende melding
+document.getElementById('comment-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const input = document.getElementById('comment').value;
+    document.getElementById('comment-section').innerHTML += `<p>${input}</p>`;
+});
